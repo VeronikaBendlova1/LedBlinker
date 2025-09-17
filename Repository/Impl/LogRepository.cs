49ac2e260e1f2178c5a9b9ee3ad38f77bd646ca8
veronika <benveronika@seznam.cz>
2025-09-17 18:14:50 +0200

    LogRepositoryImplementace

---
+++ Repository/Impl/LogRepository.cs
@@ -0,0 +1,32 @@
+﻿using LedBlinker.Data;
+using LedBlinker.Models;
+
+namespace LedBlinker.Repository.Impl
+{
+    public class LogRepository : ILogRepo
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public LogRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<Logs> AddLogAsync(Logs log)
+        {
+
+            /* _db.Logs.Add(new Logs
+            {
+                Date = DateTime.Now,
+                State = dto.State.ToString()
+            });*/
+
+            var result = _db.Logs.Add(log);
+            await _db.SaveChangesAsync();
+
+            return result.Entity;
+            
+        }
+    }
+}
