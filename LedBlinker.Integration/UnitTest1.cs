dfd017fa146c486a47a032e6e1d97452a2c38659
Jan Kocev <jan.kocev@se-va.cz>
2025-09-20 12:21:08 +0200

    move Led blinker to specific sub-directory

---
+++ LedBlinker.Integration/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace LedBlinker.Integration;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
